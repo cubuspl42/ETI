18e92eed4f383e68c8c5f6b6e5debc6de703d1ba
cubuspl42 <cubuspl42@stgn.pl>
2014-10-09 18:39:03 +0200

    lab2

---
+++ POPR/lab2/main.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <cfloat>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    double delta = b*b-4*a*c;
+    if(a == 0) {
+        // bx + c = 0
+        if(b == 0) {
+            if(c == 0) {
+                for(double x = 0; x < INFINITY; x += DBL_EPSILON) {
+                    cout << "x = " << x << " v" << endl;
+                }
+            } else {
+                cout << "brak rozwiązań" << endl;
+            }
+        } else {
+            cout << "x = " << -c/b << endl;
+        }
+    } else {
+        // ax^2 + bx + c = 0
+        if(delta < 0) {
+            cout << "delta < 0 brak rozwiązań" << endl;
+        } else {
+            double sqrtdelta = sqrt(delta);
+            double x1 = (-b-sqrtdelta)/(2*a);
+            double x2 = (-b+sqrtdelta)/(2*a);
+            cout << "x1 = " << x1 << " x2 = " << x2 << endl;
+        }
+    }
+    return 0;
+}
